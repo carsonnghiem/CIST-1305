7ded7dcb9a192cb9bdaa6dd001e1026909ee1a0d
carsonnghiem <cnghiem7458@student.gwinnetttech.edu>
2025-04-16 23:20:52 -0400

    Delete wed20250416 directory

--- wed20250416/modReadFile.py
+++
@@ -1,9 +0,0 @@
-def readFile(fileName):
-    openfile = open(fileName, 'r')
-
-    line1 = openfile.readline()
-    while openfile != '':
-        print(line1)
-        line1 = openfile.readline()
-    
-    openfile.close()
--- wed20250416/modWriteFile.py
+++
@@ -1,8 +0,0 @@
-def writeFile(fileName):
-    openfile = open(fileName, 'w')
-
-    line1 = openfile.write(input('What do you want to write? Type "x" to stop: ') + '\n')
-    while line1 != "x":
-        line1 = openfile.write(input('What do you want to write? Type "x" to stop: ') + '\n')
-    
-    openfile.close()
